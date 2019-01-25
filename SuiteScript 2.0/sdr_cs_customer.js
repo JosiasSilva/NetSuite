3ddc2fcf5fa141c0d7d88a493674b32b326d6394
Amar Kumar <amrkrcse@gmail.com>
2019-01-25 18:30:32 +0530

    Create sdr_cs_customer.js

---
+++ SuiteScript 2.0/sdr_cs_customer.js
@@ -0,0 +1,200 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define([],
+
+function() {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(context) {
+    	var customer = context.currentRecord;
+    	alert('alert');
+    	console.log('Customer ID: ', customer.getValue('entityid'));
+    	console.log('Customer EMAIL: ', customer.getValue('email'));
+    	console.log('SALES REP NAME: ', customer.getText('salesrep'));
+    	console.log('Company: ', customer.getValue('companyname'));
+    	console.log('Subsidiary: ', customer.getText('subsidiary'));
+    	
+    	//Enable check box for apply coupon code and disable coupon code initially
+    	var applyCoupon = customer.getField('custentity_sdr_apply_coupon');
+    	var couponCode = customer.getField('custentity_coupon_code');
+    	
+    	applyCoupon.isDisabled = false;
+    	couponCode.isDisabled = true;
+    	
+    	//console.log(customer.getValue('custentity_sdr_apply_coupon'));
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(context) {
+    	var customer = context.currentRecord;
+    	
+    	debugger;
+    	if(context.fieldId = 'custentity_sdr_apply_coupon'){
+    		//console.log(customer.getValue('custentity_sdr_apply_coupon'), 1);
+    		var applyCoupon = customer.getValue('custentity_sdr_apply_coupon');
+    		var couponCode = customer.getField('custentity_coupon_code');
+    	
+    		//applyCoupon ? couponCodeisDisabled = false : couponCode.isDisabled = true;
+    		
+    		if (applyCoupon){
+    			couponCode.isDisabled = false;
+    		}else{
+    			couponCode.isDisabled = true;
+    		}
+    	}
+    	//console.log(customer.getValue('custentity_sdr_apply_coupon') , 2);
+    	
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+    }
+
+    return {
+        pageInit: pageInit,
+        fieldChanged: fieldChanged
+//        postSourcing: postSourcing,
+//        sublistChanged: sublistChanged,
+//        lineInit: lineInit,
+//        validateField: validateField,
+//        validateLine: validateLine,
+//        validateInsert: validateInsert,
+//        validateDelete: validateDelete,
+//        saveRecord: saveRecord
+    };
+    
+});
